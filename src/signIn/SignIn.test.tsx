f25d089dc9421fff5c7569a0e7b864550666da7a
Monique <moniquelimacastro@gmail.com>
2024-01-09 11:00:30 +0100

    Add test for SignIn password input

---
+++ src/signIn/SignIn.test.tsx
@@ -0,0 +1,23 @@
+import {render, screen, fireEvent} from '@testing-library/react'
+import {SignIn} from './SignIn'
+import {ThemeProvider} from '@mui/material'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {theme} from '../theme'
+
+test('Should not accept passwords with less than 8 characters', async () => {
+  render(
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<SignIn />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  )
+  const passwordInput = screen.getByLabelText('Password')
+  const submitButton = screen.getByRole('button', {name: 'Sign In'})
+  fireEvent.change(passwordInput, {target: {value: 'pass'}})
+  fireEvent.click(submitButton)
+  const errorMessage = await screen.findByText('Password must be at least 8 characters long')
+  expect(errorMessage).toBeVisible()
+})
